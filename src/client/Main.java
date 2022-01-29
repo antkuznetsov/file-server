65561d5ee6f7d72554c6612913c2a6dc9b7ecb4f
Anton Kuznetsov <antkuznetsov@yandex.ru>
2022-01-29 17:38:59 +0100

    Implemented Stage 2

---
+++ src/client/Main.java
@@ -0,0 +1,30 @@
+package client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Main {
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 8080;
+
+    public static void main(String[] args) {
+        System.out.println("Client started!");
+        try (
+                Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output  = new DataOutputStream(socket.getOutputStream())
+        ) {
+            String msg = "Give me everything you have!";
+
+            output.writeUTF(msg); // sending message to the server
+            System.out.println("Sent: " + msg);
+
+            String receivedMsg = input.readUTF(); // response message
+            System.out.println("Received: " + receivedMsg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/server/Main.java
+++ src/server/Main.java
@@ -1,7 +1,18 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Main {
+    private static final int PORT = 8080;
+
     public static void main(String[] args) {
-        new App();
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            System.out.println("Server started!");
+            Session session = new Session(server.accept());
+            session.start(); // it does not block current thread
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
